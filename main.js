e63b3929c5aa7ce893f65c46efe385db98e279b4
csmetcalfe <chris.s.metcalfe@gmail.com>
2017-10-07 01:43:22 -0700

    Be more specific with query selector

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
 maxGasPriceInGwei = 500;
 console.log('Gas Pump Extension - upping max Gas Price to ' + maxGasPriceInGwei);
-document.querySelector('[ng-model]').setAttribute('max', maxGasPriceInGwei);
+document.querySelector('[ng-model="gas.value"]').setAttribute('max', maxGasPriceInGwei);
 
